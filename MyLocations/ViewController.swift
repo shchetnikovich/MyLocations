adbc90fa09e8752beb0d8d0c6e7d0e85d1a48d08
shchetnikovich <shchetnikovich@gmail.com>
2023-01-20 15:59:40 +0300

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Anton Shchetnikovich on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

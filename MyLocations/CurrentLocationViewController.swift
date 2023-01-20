f3ab75e4b2c4d0ab15879c533057bbb9fd208e5c
Anton Shchetnikovich <shchetnikovich@gmail.com>
2023-01-20 16:31:55 +0300

    Add labels
    Добавил лэйбли в сториборде, связал с аутлетами в CurrentLocationViewController.

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  MyLocations
-//
-//  Created by Anton Shchetnikovich on 20.01.2023.
-//
-
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    
+    // MARK: - Actions
+    
+    @IBAction func getLocation() {
+print("hello")
+    }
 
 }
 

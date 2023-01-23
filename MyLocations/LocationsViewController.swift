e337780d76c61e075dd57ea8ef861edaf754bb4b
Anton Shchetnikovich <shchetnikovich@gmail.com>
2023-01-23 16:28:32 +0300

    Add Title for Header in Locations row

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -10,17 +10,21 @@ class LocationsViewController: UITableViewController {
         let fetchRequest = NSFetchRequest<Location>()
         let entity = Location.entity()
         fetchRequest.entity = entity
-        let sortDescriptor = NSSortDescriptor(
-            key: "date",
-            ascending: true)
-        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let sort1 = NSSortDescriptor(key: "category", ascending: true)
+        let sort2 = NSSortDescriptor(key: "date", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sort1, sort2]
+        
         fetchRequest.fetchBatchSize = 20
+        
         let fetchedResultsController = NSFetchedResultsController(
             fetchRequest: fetchRequest,
             managedObjectContext: self.managedObjectContext,
-            sectionNameKeyPath: nil,
+            sectionNameKeyPath: "category",
             cacheName: "Locations")
         fetchedResultsController.delegate = self
+        
         return fetchedResultsController
     }()
     
@@ -76,7 +80,22 @@ class LocationsViewController: UITableViewController {
             } catch {
                 fatalCoreDataError(error)
             }
-        } }
+        }
+    }
+    
+    override func numberOfSections(
+        in tableView: UITableView
+    ) -> Int {
+        return fetchedResultsController.sections!.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        titleForHeaderInSection section: Int
+    ) -> String? {
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.name
+    }
     
     // MARK: - Navigation
     
@@ -93,7 +112,6 @@ class LocationsViewController: UITableViewController {
             }
         }
     }
-    
 }
 
 

d4cf4040841ad3265d1033e5c9693015efe8e27d
Anton Shchetnikovich <shchetnikovich@gmail.com>
2023-01-23 18:06:25 +0300

    Add Foto feature

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -17,6 +17,9 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet var longitudeLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var addPhotoLabel: UILabel!
+    @IBOutlet var imageHeight: NSLayoutConstraint!
     
     
     var coordinate = CLLocationCoordinate2D(
@@ -44,6 +47,8 @@ class LocationDetailsViewController: UITableViewController {
     }
     
     var descriptionText = ""
+    
+    var image: UIImage?
     
     
     override func viewDidLoad() {
@@ -89,6 +94,9 @@ class LocationDetailsViewController: UITableViewController {
         didSelectRowAt indexPath: IndexPath ){
             if indexPath.section == 0 && indexPath.row == 0 {
                 descriptionTextView.becomeFirstResponder()
+            } else if indexPath.section == 1 && indexPath.row == 0 {
+                tableView.deselectRow(at: indexPath, animated: true)
+                pickPhoto()
             }
         }
     
@@ -131,6 +139,14 @@ class LocationDetailsViewController: UITableViewController {
             return
         }
         descriptionTextView.resignFirstResponder()
+    }
+    
+    func show(image: UIImage) {
+        imageView.image = image
+        imageView.isHidden = false
+        addPhotoLabel.text = ""
+        imageHeight.constant = 260
+        tableView.reloadData()
     }
     
     // MARK: - Navigation
@@ -192,4 +208,80 @@ class LocationDetailsViewController: UITableViewController {
         categoryLabel.text = categoryName
     }
     
+}
+
+extension LocationDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - Image Helper Methode
+    
+    func takePhotoWithCamera() {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - Image Picker Delegates
+    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info:
+        [UIImagePickerController.InfoKey: Any]
+    ){
+        image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        
+        if let theImage = image {
+            show(image: theImage)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(
+        _ picker: UIImagePickerController
+    ){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func pickPhoto() {
+      if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        showPhotoMenu()
+      } else {
+        choosePhotoFromLibrary()
+      }
+    }
+    
+    func showPhotoMenu() {
+      let alert = UIAlertController(
+        title: nil,
+        message: nil,
+        preferredStyle: .actionSheet)
+      let actCancel = UIAlertAction(
+        title: "Отмена",
+        style: .cancel,
+        handler: nil)
+      alert.addAction(actCancel)
+      let actPhoto = UIAlertAction(
+        title: "Сделать фотографию",
+        style: .default) { _ in
+            self.takePhotoWithCamera()
+        }
+      alert.addAction(actPhoto)
+      let actLibrary = UIAlertAction(
+        title: "Выбрать из Галереи",
+        style: .default) { _ in
+            self.choosePhotoFromLibrary()
+        }
+          alert.addAction(actLibrary)
+          present(alert, animated: true, completion: nil)
+        }
 }
--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -6,7 +6,20 @@ class MapViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
     
-    var managedObjectContext: NSManagedObjectContext!
+    var managedObjectContext: NSManagedObjectContext! {
+        didSet {
+            NotificationCenter.default.addObserver(
+                forName:
+                    Notification.Name.NSManagedObjectContextObjectsDidChange,
+                object: managedObjectContext,
+                queue: OperationQueue.main
+            ) { _ in
+                if self.isViewLoaded {
+                    self.updateLocations()
+                }
+            }
+        }
+    }
     
     var locations = [Location]()
     
@@ -89,6 +102,26 @@ class MapViewController: UIViewController {
         return mapView.regionThatFits(region)
     }
     
+    @objc func showLocationDetails(_ sender: UIButton) {
+        performSegue(withIdentifier: "EditLocation", sender: sender)
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(
+        for segue: UIStoryboardSegue,
+        sender: Any?
+    ){
+        if segue.identifier == "EditLocation" {
+            let controller = segue.destination as!
+            LocationDetailsViewController
+            controller.managedObjectContext = managedObjectContext
+            let button = sender as! UIButton
+            let location = locations[button.tag]
+            controller.locationToEdit = location
+        }
+    }
+    
     // MARK: - Actions
     
     @IBAction func showUser() {
@@ -108,4 +141,46 @@ class MapViewController: UIViewController {
 }
 
 extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(           //  Custom Pins on Map
+        _ mapView: MKMapView,
+        viewFor annotation: MKAnnotation
+    ) -> MKAnnotationView? {
+        guard annotation is Location else { return nil }
+        
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationView( withIdentifier: identifier)
+        if annotationView == nil {
+            let pinView = MKPinAnnotationView(
+                annotation: annotation,
+                reuseIdentifier: identifier)
+            
+            pinView.isEnabled = true
+            pinView.canShowCallout = true
+            pinView.animatesDrop = false
+            pinView.pinTintColor = UIColor(
+                red: 0.32,
+                green: 0.82,
+                blue: 0.4,
+                alpha: 1)
+            
+            let rightButton = UIButton(type: .detailDisclosure)
+            rightButton.addTarget(
+                self,
+                action: #selector(showLocationDetails(_:)),
+                for: .touchUpInside)
+            pinView.rightCalloutAccessoryView = rightButton
+            annotationView = pinView
+        }
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+            let button = annotationView.rightCalloutAccessoryView as!
+            UIButton
+            if let index = locations.firstIndex(of: annotation as!
+                                                Location) {
+                button.tag = index
+            }
+        }
+        return annotationView
+    }
 }
